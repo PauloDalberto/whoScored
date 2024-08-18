a61e2ee7422ed199cca8b317e3b7edf9f7b8232e
Paulo Henrique Dalberto <phdalberto2016@gmail.com>
2024-08-18 13:56:35 -0300

    feat: add search to player

--- src/app/pages/brasileirao/page.tsx
+++ src/app/pages/brasileirao/page.tsx
@@ -1,17 +1,115 @@
-import Image from 'next/image';
-import './brasileirao.css'
+'use client';
 
-export default function Brasileirao(){
+import Image from 'next/image';
+import './brasileirao.css';
+import { useState, useEffect } from 'react';
+
+export default function Brasileirao() {
+  const [playerName, setPlayerName] = useState<string>('');
+  const [players, setPlayers] = useState<any[]>([]);
+  const [selectedPlayer, setSelectedPlayer] = useState<any | null>(null);
+
+  const leagueId = 39;
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'x-rapidapi-key': '34ba5f18f2msh826a9705ea11cb0p16a5b2jsne38545fbbf18',
+      'x-rapidapi-host': 'api-football-v1.p.rapidapi.com'
+    }
+  };
+
+  useEffect(() => {
+    if (playerName) {
+      const fetchPlayers = async () => {
+        const url = `https://api-football-v1.p.rapidapi.com/v3/players?search=${encodeURIComponent(playerName)}&league=${leagueId}`;
+
+        try {
+          const response = await fetch(url, options);
+          const result = await response.json();
+
+          if (result.response && result.response.length > 0) {
+            setPlayers(result.response);
+          } else {
+            setPlayers([]);
+          }
+        } catch (error) {
+          console.error('Erro ao buscar os dados:', error);
+        }
+      };
+
+      fetchPlayers();
+    } else {
+      setPlayers([]);
+    }
+  }, [playerName]);
+
+  function handlePlayer(event: React.ChangeEvent<HTMLInputElement>) {
+    setPlayerName(event.target.value);
+  }
+
+  function handleSelection(playerData: Player) {
+    setSelectedPlayer(playerData);
+  }
 
   return (
     <section className="container">
-      <div className='content'>
-        <Image src="/provisorio.png" alt="Logo do vant" width={550} height={300} priority={true} />
+      <div className="content">
+        <Image src="/provisorio.png" alt="Logo do vant" width={550} height={300} priority />
 
-        <input type="text" placeholder='3 tentivas possiveis' className='search'/>
+        <input 
+          type="text" 
+          placeholder="1 tentativa de 2 possiveis" 
+          className="search" 
+          onChange={handlePlayer} 
+          value={playerName}
+        />
 
-        
+        {players.length > 0 && (
+          <div className="autocomplete-suggestions">
+            {players.map((playerData) => (
+              <div 
+                key={playerData.player.id} 
+                className="autocomplete-suggestion" 
+                onClick={() => handleSelection(playerData)}
+              >
+                <Image src={playerData.player.photo} alt='' className="player-photo" width={50} height={50} />
+                <span>{playerData.player.name}</span>
+              </div>
+            ))}
+          </div>
+        )}
+
+        {selectedPlayer && (
+          <div className='result'>
+            <h3>{selectedPlayer.player.name}</h3>
+
+            <div className='container-items'>
+              <div className='item-wrapper'>
+                <div className='item-content'>
+                  <p>{selectedPlayer.player.nationality}</p>
+                </div>
+                <h3>NAT</h3>
+              </div>
+
+              <div className='item-wrapper'>
+                <div className='item-content'>
+                  <Image src={selectedPlayer.statistics[0].team.logo} alt={selectedPlayer.player.name} width={50} height={50} />
+                </div>
+                <h3>TEAM</h3>
+              </div>
+
+              <div className='item-wrapper'>
+                <div className='item-content'>
+                  <p>{selectedPlayer.statistics[0].games.position}</p>
+                </div>
+                <h3>position</h3>
+              </div>
+
+            </div>
+          </div>
+        )}
       </div>
     </section>
-  )
+  );
 }
