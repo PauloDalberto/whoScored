74b7fadf552b4417e33d374f022ed23d8e4b1ecb
Paulo Henrique Dalberto <phdalberto2016@gmail.com>
2024-08-21 13:08:57 -0300

    feat: add modal

---
+++ src/components/modal/ModalSuccess.tsx
@@ -0,0 +1,16 @@
+import './home.css';
+
+
+export default function ModalSuccess(){
+  return(
+    <section className='modal-container'>
+    <div className='modal'>
+      <div className='content'>
+        <h1>Parabéns</h1>
+
+        O jogador era: 
+      </div>
+    </div>
+  </section>
+  )
+}
