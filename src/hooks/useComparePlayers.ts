c420d657f9b7715cee7f63badce1211d088ce4a7
Paulo Henrique Dalberto <phdalberto2016@gmail.com>
2024-08-22 19:39:13 -0300

    feat: add change on midnight, and change club to age

--- src/hooks/useComparePlayers.ts
+++ src/hooks/useComparePlayers.ts
@@ -1,18 +1,28 @@
 import players from '@/data/playerData.json';
 
+let correctPlayerId = 1;
+let lastUpdate = new Date().toLocaleDateString(); 
+
 export function comparePlayerData(playerData: Player): ComparisonResult | null {
-  const correctPlayer = players.find((player) => player.id === 2);
+  const currentDate = new Date().toLocaleDateString();
+
+  if (currentDate !== lastUpdate) {
+    correctPlayerId += 1; 
+    lastUpdate = currentDate; 
+  }
+
+  const correctPlayer = players.find((player) => player.id === correctPlayerId);
 
   if (!correctPlayer) return null;
 
   const isNationalityCorrect = playerData.player.nationality === correctPlayer.nationality;
-  const isClubCorrect = playerData.statistics[0].team.name === correctPlayer.club;
+  const isAgeCorrect = playerData.player.age === correctPlayer.age;
   const isPositionCorrect = playerData.statistics[0].games.position === correctPlayer.position;
 
   return {
     isNationalityCorrect,
-    isClubCorrect,
+    isAgeCorrect,
     isPositionCorrect,
-    isCorrect: isNationalityCorrect && isClubCorrect && isPositionCorrect
+    isCorrect: isNationalityCorrect && isAgeCorrect && isPositionCorrect,
   };
 }
