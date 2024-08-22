c7ee736321e1f9f196051b3d1e3e48489155fee4
Paulo Henrique Dalberto <phdalberto2016@gmail.com>
2024-08-22 10:46:35 -0300

    feat: create modal error and success and refac to unic css file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import BrasilSvg from '../../public/svg/countries/Brasil';
 import IngleterraSvg from '../../public/svg/countries/Inglaterra';
 import ItaliaSvg from '../../public/svg/countries/Italia';
 import './home.css';
+import ModalSuccess from '@/components/modal/ModalSuccess';
+import ModalError from '@/components/modal/ModalError';
 
 export default function Home() {
   return (
@@ -16,6 +18,8 @@ export default function Home() {
         <li><Link className='link' href={'/pages/premierLeague'}><IngleterraSvg /><p>Premier League</p></Link></li>
         <li><Link className='link' href={'/pages/serieA'}><ItaliaSvg /><p>Serie A</p></Link></li>
       </ul>
+
+      <ModalError />
     </main>
   );
 }
--- src/components/modal/ModalError.tsx
+++ src/components/modal/ModalError.tsx
@@ -4,20 +4,20 @@ import './modal.css';
 import Image from 'next/image';
 import { useFetchPlayers } from '@/hooks/useEffectPlayers';
 
-export default function ModalSuccess(){
+export default function ModalError(){
   const players = useFetchPlayers({ playerName: 'neymar', leagueId: 71 });
 
 //aqui nos jogaodres mudar para o player certo conforme o erro:
   return(
     <section className='modal-container'>
-    <div className='modal modal-success'>
+    <div className='modal modal-error'>
       <div className='close'>
         X
       </div>
-      <div className='content content-success'>
-        <h1>Parabéns</h1>
+      <div className='content content-error'>
+        <h1>Foi por pouco</h1>
 
-        Você acertou o jogador 
+        O jogador era:
 
         {players.map((playerData) => (
           <div className='player' key={playerData.player.id}>
