0ba01838e746d30cee58360a9deb761e1e9d3070
Paulo Henrique Dalberto <phdalberto2016@gmail.com>
2024-08-22 12:38:06 -0300

    fix: change modal

--- src/components/modal/ModalError.tsx
+++ src/components/modal/ModalError.tsx
@@ -3,32 +3,35 @@
 import './modal.css';
 import Image from 'next/image';
 import { useFetchPlayers } from '@/hooks/useEffectPlayers';
+import { useState } from 'react';
 
 export default function ModalError(){
+  const [isOpen, setIsOpen] = useState(true);
   const players = useFetchPlayers({ playerName: 'neymar', leagueId: 71 });
 
-//aqui nos jogaodres mudar para o player certo conforme o erro:
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  if (!isOpen) return null;
+  
   return(
-    <section className='modal-container'>
-    <div className='modal modal-error'>
-      <div className='close'>
-        X
+    <section className='modal-container' onClick={closeModal}>
+      <div className='modal modal-error'>
+        <div className='close' onClick={closeModal}>
+          X
+        </div>
+        <div className='content content-error'>
+          <h1>Foi por pouco</h1>
+          O jogador era:
+          {players.map((playerData) => (
+            <div className='player' key={playerData.player.id}>
+              <Image src={playerData.player.photo} width={70} height={70} alt='player'/>
+              <p>{playerData.player.name}</p>
+            </div>
+          ))}
+        </div>
       </div>
-      <div className='content content-error'>
-        <h1>Foi por pouco</h1>
-
-        O jogador era:
-
-        {players.map((playerData) => (
-          <div className='player' key={playerData.player.id}>
-            <Image src={playerData.player.photo} width={70} height={70} alt='player'/>
-            <p>{playerData.player.name}</p>
-          </div>
-        ))}
-
-        
-      </div>
-    </div>
   </section>
   )
 }
--- src/components/modal/ModalSuccess.tsx
+++ src/components/modal/ModalSuccess.tsx
@@ -3,32 +3,35 @@
 import './modal.css';
 import Image from 'next/image';
 import { useFetchPlayers } from '@/hooks/useEffectPlayers';
+import { useState } from 'react';
 
-export default function ModalSuccess(){
+export default function ModalSuccess() {
+  const [isOpen, setIsOpen] = useState(true);
   const players = useFetchPlayers({ playerName: 'neymar', leagueId: 71 });
 
-//aqui nos jogaodres mudar para o player certo conforme o erro:
-  return(
-    <section className='modal-container'>
-    <div className='modal modal-success'>
-      <div className='close'>
-        X
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  if (!isOpen) return null;
+
+  return (
+    <section className='modal-container' onClick={closeModal}>
+      <div className='modal modal-success'>
+        <div className='close' onClick={closeModal}>
+          X
+        </div>
+        <div className='content content-success'>
+          <h1>Parabéns</h1>
+          Você acertou o jogador 
+          {players.map((playerData) => (
+            <div className='player' key={playerData.player.id}>
+              <Image src={playerData.player.photo} width={70} height={70} alt='player'/>
+              <p>{playerData.player.name}</p>
+            </div>
+          ))}
+        </div>
       </div>
-      <div className='content content-success'>
-        <h1>Parabéns</h1>
-
-        Você acertou o jogador 
-
-        {players.map((playerData) => (
-          <div className='player' key={playerData.player.id}>
-            <Image src={playerData.player.photo} width={70} height={70} alt='player'/>
-            <p>{playerData.player.name}</p>
-          </div>
-        ))}
-
-        
-      </div>
-    </div>
-  </section>
-  )
+    </section>
+  );
 }
