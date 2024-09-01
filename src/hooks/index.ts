9125e5f34cfcb20ab6332322daa80a05e77f6000
Paulo Henrique Dalberto <phdalberto2016@gmail.com>
2024-09-01 20:29:36 -0300

    fix: remove unsed functions

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,7 +2,5 @@ export { usePlayerHandlers } from './usePlayerhandlers';
 export { useFetchPlayers } from './useEffectPlayers';
 export { showVideo } from './useShowVideo';
 export { useGameState } from './useGameState';
-export { isMidnight } from './isMidnight';
-export { updatePlayerIdAtMidnight } from './uptadePlayerId';
 export { comparePlayerData } from './useComparePlayers';
 
--- src/hooks/isMidnight.ts
+++
@@ -1,8 +0,0 @@
-export function isMidnight(): boolean {
-  const currentTime = new Date();
-  const hours = currentTime.getHours();
-  const minutes = currentTime.getMinutes();
-  const seconds = currentTime.getSeconds();
-
-  return hours === 0 && minutes === 0 && seconds === 0;
-}
